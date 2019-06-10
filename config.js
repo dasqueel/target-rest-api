b438fa8346c6254f69d5ec7ff1d2ceec4c47ec6e
dasqueel <barduson.neil@gmail.com>
2019-06-09 21:34:41 -0500

    lower case models/product.js file.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.mongoURI = {
   development_local: "mongodb://localhost/target-dev",
   test_local: "mongodb://localhost/target-test",
   development_remote: process.env.REMOTE_DB_PATH,
-  development_remote: process.env.REMOTE_DB_PATH_TEST
+  test_remote: process.env.REMOTE_DB_PATH_TEST
 }
 
 module.exports = config
